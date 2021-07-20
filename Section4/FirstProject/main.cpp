bc77a00585cd059af7ef0775575d2e0161ae14db
vikas chinchansur <vikas.chinchansur@gmail.com>
2021-07-20 19:23:34 +1000

    Add files via upload

---
+++ Section4/FirstProject/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main(){
+    int fav_num;
+    std::cout << "Enter your fav number between 1 to 100: ";
+    std::cin >> fav_num;
+    std::cout << "Amazing thats my Fav number too!!!" << std::endl;
+    return 0;
+}
